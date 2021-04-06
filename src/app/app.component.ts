a5b2cf550cb547d00cbae7f774a4b2ea91864d36
Atchukorn Tonsiriskulvong <[tatchukorn@gmail.com]>
2021-04-06 22:43:50 +0700

    text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'part3Homework1';
+
+  currentTab! : string;
+
+  selectTab(tab : string) {
+    this.currentTab = tab;
+  }
+
 }
